d39eed5feb4061bf150dda758153cce609b3c3d6
808248 <archinrb@gmail.com>
2022-05-26 19:39:59 +0530

    Initial

---
+++ materialapp/src/App.js
@@ -0,0 +1,10 @@
+import Grid from '@mui/material/Grid';
+import * as React from 'react';import Container from '@mui/material/Container';
+import Full from "./Full/Full";
+function App() {
+  return (
+    <Full />
+  );
+}
+
+export default App;
